26ff5f46380f5300924e8a200b560d0c258cce6e
songgy <1661745274@qq.com>
2018-08-28 15:20:04 +0800

    源码

---
+++ Conf/config.php
@@ -0,0 +1,68 @@
+<?php
+
+    return array(
+        /* 数据库设置 */
+        'DB_TYPE'               =>  'mysqli',     // 数据库类型
+        'DB_HOST'               =>  '120.77.211.154', // 服务器地址
+        'DB_NAME'               =>  'ln_beidanci',          // 数据库名
+        'DB_USER'               =>  'xijiushop',      // 用户名
+        'DB_PWD'                =>  'xijiushop',          // 密码
+        'DB_PORT'               =>  '3306',        // 端口
+        'DB_PREFIX'             =>  'ln_',    // 数据库表前缀
+        'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
+
+        /* XSS过滤 */
+        'DEFAULT_FILTER'    => 'trim,filter_xss',
+        
+        /* 模板路径配置 */
+        // 'SHOW_PAGE_TRACE' => true,
+        'TMPL_PARSE_STRING' => array(
+            '__PUBLIC__'    => '/Public',
+            '__STATIC__'    => '/Static',
+            '__ADMIN__'     => '/Application/Admin/Statics',
+            '__SHOP__'     => '/Application/Shop/Statics',
+            '__HOME__'     => '/Application/Home/Statics',
+            '__UPLOADS__'   => '/Uploads/',
+        ),
+
+        'URL_HTML_SUFFIX'       =>  '',  // URL伪静态后缀设置
+
+        //'MODULE_ALLOW_LIST' => array ('Home','Core','Admin', 'Payment', 'Api','Shop'),
+        'DEFAULT_MODULE' => 'Admin',
+
+        /* 图片上传相关配置 */
+        'PICTURE_UPLOAD' => array(
+            'mimes'    => '', //允许上传的文件MiMe类型
+            'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
+            'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
+            'autoSub'  => true, //自动子目录保存文件
+            'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
+            'rootPath' => './Uploads/Picture/', //保存根路径
+            'savePath' => '', //保存路径
+            'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+            'saveExt'  => '', //文件保存后缀，空则使用原后缀
+            'replace'  => false, //存在同名是否覆盖
+            'hash'     => true, //是否生成hash编码
+            'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
+        ), //图片上传相关配置（文件上传类配置）
+        // 前台所用图片上传目录
+        'UPLOAD_PICTURE_ROOT' => '/Uploads/Picture',
+        /* UPLOAD上传图片路径调用 */
+        'UPLOAD_URL' => '/Uploads/',
+        //上传大小
+        'UPLOAD_SIZE' => 5*1024*1024,
+        'UPLOAD_VIDEO_SIZE' => 20*1024*1024,
+        /* 图片存放服务器地址 */
+        'IMG_SERVER' => 'http://yimall.host5.liuniukeji.com',
+        // app默认头像
+        'DEFAULT_PHOTO' => '/Public/images/avatr.png',
+
+
+        /*自动登录需要使用的加密KEY值*/
+        'ENCTYPTION_KEY' => 'LNShop!@#$',
+        'AUTO_LOGIN_TIME' => 604800, //一周免登录时间
+
+        'TMPL_ACTION_ERROR'     =>  THINK_PATH.'Tpl/dispatch_jump.tpl', // 默认错误跳转对应的模板文件
+        'TMPL_ACTION_SUCCESS'   =>  THINK_PATH.'Tpl/dispatch_jump.tpl', // 默认成功跳转对应的模板文件
+
+    );
