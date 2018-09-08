6314f70c0ef9ae8d7d96e6caefdd8de290db7ed0
tianzj <2413877061@qq.com>
2018-09-08 15:03:48 +0800

    1

--- Application/Api/Controller/PublicApiController.class.php
+++ Application/Api/Controller/PublicApiController.class.php
@@ -28,10 +28,10 @@ class PublicApiController extends ApiCommonController {
     public function getHomeData(){
         $keywords = I('keywords', '', 'trim');
         $city_id = I('city_id', 0, 'intval');
-        $where = array('city_id' => $city_id, 'disabled' => 1);
+        $where = array('city_id' => $city_id, 'a.disabled' => 1);
         if($keywords) $where['question_title'] = array('like', '%'.$keywords.'%');
         $model = D('Admin/Question');
-        $field = 'u.nickname,u.head_pic,a.id,a.like_number,a.browse_number,a.answer_number,a.add_time,a.title';
+        $field = 'u.nickname,u.head_pic,a.id,a.like_number,a.browse_number,a.answer_number,a.add_time,a.question_title';
         $question_list = $model->getQuestionList($where, $field);
         foreach($question_list as &$val){
             $val['nickname'] = strval($val['nickname']);
