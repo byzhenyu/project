a2df5b08880f08067b41199fedc474464c0a53f5
jicy <510434563@qq.com>
2018-09-11 16:01:34 +0800

    RecruitModel

--- Application/Admin/Model/RecruitModel.class.php
+++ Application/Admin/Model/RecruitModel.class.php
@@ -11,9 +11,20 @@ class RecruitModel extends Model {
 //    protected $updateFields = array('position_name', 'sort');
     protected $selectFields = array('*');
     protected $_validate = array(
-        array('position_name', 'require', '职位名称不能为空！', 1, 'regex', 3),
-        array('sort', 'require', '排序不能为空！', 1, 'regex', 3),
-        array('position_name', 'checkPositionLength', '职位名称不能超过30字！', 2, 'callback', 3),
+        array('hr_user_id', 'require', '发布人不能为空', 1, 'regex', 3),
+        array('position_id', 'require', '请选择悬赏职位', 1, 'regex', 3),
+        array('commission', 'number', '请填写悬赏佣金', 1, 'regex', 3),
+        array('recruit_num', 'number', '请填写招聘人数', 1, 'regex', 3),
+        array('nature', 'require', '发布人不能为空', 1, 'regex', 3),
+        array('sex', array(0,1,2), '性别字段有误', 1, 'in', 3),
+        array('degree', 'require', '请选择学历要求', 1, 'regex', 3),
+        array('language_ability', '1,255', '请填写语言要求', 1, 'length', 3),
+        array('experience', 'require', '请选择工作经验', 1, 'regex', 3),
+        array('job_area', 'require', '请选择工作地区', 1, 'regex', 3),
+        array('base_pay', 'require', '请填写基本工资', 1, 'regex', 3),
+        array('merit_pay', 'require', '请填写绩效工资', 1, 'regex', 3),
+        array('welfare', 'require', '请选择福利', 1, 'regex', 3),
+
     );
 
     /**
@@ -34,21 +45,58 @@ class RecruitModel extends Model {
      * @param $order string 排序顺序
      * @return mixed
      */
-    public function getRecruitList($where, $field = false, $order = 'sort', $isPage = true){
+    public function getRecruitList($where, $field = false, $order = 'id desc', $isPage = true){
         if(!$field) $field = '*';
         if(!$isPage) return $this->where($where)->field($field)->order($order)->select();
         $count = $this->where($where)->count();
+
         $page = get_web_page($count);
         $list = $this->where($where)->limit($page['limit'])->field($field)->order($order)->select();
-
+        foreach ($list as $k=>$v) {
+            $list[$k]['add_time'] = time_format($v['add_time']);
+            $list[$k]['commission'] = fen_to_yuan($v['commission']);
+            $list[$k]['sex'] = getSexInfo($v['sex']);
+        }
         return array('info' => $list, 'page' => $page['page']);
     }
 
     //添加操作前的钩子操作
     protected function _before_insert(&$data, $option){
+        $data['hr_user_id'] = UID;
+        $data['commission'] = yuan_to_fen($data['commission']);
+        $data['add_time'] = NOW_TIME;
     }
     //更新操作前的钩子操作
     protected function _before_update(&$data, $option){
     }
+
+    /**
+     * 返回平均值
+     * @param $where
+     */
+    public function getAverageValue($where) {
+        $info = $this->where($where)->avg('commission');
+        return fen_to_yuan($info);
+    }
+
+    /**
+     * 详情
+     */
+    public function getDetail($where) {
+
+        $info = $this->where($where)->find();
+        $wordArr = C('WORK_NATURE');
+        $exp = C('WORK_EXP');
+        $sexArr = array('0'=>'不限','1'=>'男','2'=>'女');
+        $degreeArr = M('Education')->where()->getField('id,education_name', true);
+        $tags = M('Tags')->where(array('tags_type'=>3))->getField('id, position_name',true);
+        $info['sex'] = $exp[$info['sex']];
+        $info['nature'] = $wordArr[$info['nature']];
+        $info['degree'] = $degreeArr[$info['degree']];
+        $info['commission'] = fen_to_yuan($info['commission']);
+        $info['add_time'] = time_format($info['add_time']);
+        return $info;
+    }
+
 
 }
