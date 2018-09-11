0286675f41c9f8273eaaebcaf2ed15386953a212
tianzj <2413877061@qq.com>
2018-09-11 10:49:23 +0800

    1

--- Application/Admin/Model/ResumeEvaluationModel.class.php
+++ Application/Admin/Model/ResumeEvaluationModel.class.php
@@ -26,6 +26,15 @@ class ResumeEvaluationModel extends Model {
             $this->error = '您已经评价过该简历！';
             return false;
         }
+        $valid = array('skill_score' => '技能得分', 'major_score' => '专业得分', 'chat_score' => '沟通得分', 'appearance_score' => '仪容得分', 'innovate_score' => '创新得分', 'analysis_score' => '分析得分');
+        $keys = array_keys($valid);
+        foreach($keys as &$val){
+            if(!in_array($data[$val], array(1,2,3,4,5))){
+                $this->error = $valid[$val].'值范围不对！';
+                return false;
+                break;
+            }
+        }
         $data['add_time'] = NOW_TIME;
     }
     protected function _before_update(&$data, $option){
@@ -42,7 +51,7 @@ class ResumeEvaluationModel extends Model {
         $skill_score_number = $major_score_number = $char_score_number = $appearance_score_number = $innovate_score_number = $analysis_score_number = 0;
         foreach($evaluation as &$val){
             foreach($fieldArray as &$value){
-                $$value += $val[$$value];
+                $$value += $val[$value];
                 $temp_number_field = $value.'_number';
                 $$temp_number_field ++;
             }
@@ -54,7 +63,7 @@ class ResumeEvaluationModel extends Model {
             $t_number = $$t_field;
             $t_score = $$v;
             $avg_score = round($t_score / $t_number);
-            $ret_arr[] = array($v => $avg_score);
+            $ret_arr[$v] = $avg_score;
         }
         unset($v);
         return $ret_arr;
