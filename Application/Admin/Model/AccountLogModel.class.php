6b5e39c47e0eb7d4111c4335b6807f4908a36e1f
jicy <510434563@qq.com>
2018-09-12 09:34:48 +0800

    资金记录模型

---
+++ Application/Admin/Model/AccountLogModel.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 资金记录模型
+ */
+namespace Admin\Model;
+
+use Think\Model;
+
+class AccountLogModel extends Model {
+    protected $insertFields = array('user_id', 'user_money', 'frozen_money', 'pay_points','change_time','change_desc','change_type','order_sn');
+    protected $updateFields = array('user_id', 'user_money', 'frozen_money', 'pay_points','change_time','change_desc','change_type','order_sn');
+    protected $selectFields = array('log_id','user_id', 'user_money', 'frozen_money', 'pay_points','change_time','change_desc','change_type','order_sn');
+    protected $_validate = array(
+
+    );
+
+    protected function _before_insert(&$data, $option){
+
+    }
+
+    /**
+     * 资金变动记录
+     * @param $where
+     * @param string $field
+     * @param string $order
+     */
+    public function getAccountLogByPage($where,$field='',$order="log_id desc") {
+        if (!$field) {
+            $field = $this->selectFields;
+        }
+        $count = $this->where($where)->count();
+        $page = get_page($count);
+        $info = $this->field($field)
+            ->where($where)
+            ->order($order)
+            ->limit($page['limit'])
+            ->select();
+        return array(
+            'info'=>$info,
+            'page'=>$page['page']
+        );
+    }
+}
