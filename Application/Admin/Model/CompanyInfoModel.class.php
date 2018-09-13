e05e69ebbf4eadae937fbcbfe99dafee90d61298
jicy <510434563@qq.com>
2018-09-13 15:40:44 +0800

    公司资料模型

---
+++ Application/Admin/Model/CompanyInfoModel.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * 公司资料模型
+ */
+namespace Admin\Model;
+
+use Think\Model;
+
+class CompanyInfoModel extends Model {
+    protected $insertFields = array('user_id,company_name,company_size,company_nature,company_mobile,company_email,company_industry,company_address,company_pic');
+    protected $updateFields = array('user_id,company_name,company_size,company_nature,company_mobile,company_email,company_industry,company_address,company_pic');
+    protected $_validate = array(
+        array('company_name', 'require', '公司名称不能为空！', 1, 'regex', 3),
+        array('company_name', '1,100', '公司名称保持在1-100字！', 1, 'regex', 3),
+        array('company_size', 'require', '请选择公司规模！', 1, 'regex', 3),
+        array('company_nature', 'require', '请选择公司性质！', 1, 'regex', 3),
+        array('company_mobile', 'require', '请填写公司联系电话！', 1, 'regex', 3),
+        array('company_email', 'require', '请填写公司联系邮箱！', 1, 'regex', 3),
+        array('company_industry', 'require', '公司所属行业不能为空！', 1, 'regex', 3),
+        array('company_address', 'require', '请填写公司地址！', 1, 'regex', 3),
+        array('company_address', '1,150', '公司名称保持在1-150字！', 1, 'regex', 3),
+    );
+
+    /**
+     * @desc 获取公司列表
+     * @param $where
+     * @param bool $field
+     * @param string $order
+     * @return array
+     */
+    public function getCompanyList($where, $field = false, $order = 'id desc'){
+        if(!$field) $field = '*';
+        $number = $this->where($where)->count();
+        $page = get_web_page($number, 20);
+        $list = $this->where($where)->field($field)->order($order)->limit($page['limit'])->select();
+        return array(
+            'info' => $list,
+            'page' => $page['page']
+        );
+    }
+
+    //添加操作前的钩子操作
+    protected function _before_insert(&$data, $option){
+
+    }
+    //更新操作前的钩子操作
+    protected function _before_update(&$data, $option){
+    }
+
+}
