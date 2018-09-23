ad2d83ca3be7e0dddda264d14a2d434ca2fa2265
tianzj <2413877061@qq.com>
2018-09-23 09:40:00 +0800

    1

--- Application/Api/Controller/PublicApiController.class.php
+++ Application/Api/Controller/PublicApiController.class.php
@@ -31,7 +31,7 @@ class PublicApiController extends ApiCommonController
     {
         $keywords = I('keywords', '', 'trim');
         $city_id = I('city_id', 0, 'intval');
-        $where = array('city_id' => $city_id, 'a.disabled' => 1);
+        $where = array('city_name' => $city_id, 'a.disabled' => 1);
         if ($keywords) $where['question_title'] = array('like', '%' . $keywords . '%');
         $model = D('Admin/Question');
         $field = 'u.nickname,u.head_pic,a.id,a.like_number,a.browse_number,a.answer_number,a.add_time,a.question_title';
