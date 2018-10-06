18df3b69e84132eb2a010cb5f0bbf15dcabc4d88
jicy <510434563@qq.com>
2018-10-06 15:27:02 +0800

    wxNotify

---
+++ Application/Payment/Controller/WxPayController.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 微信支付回调
+ * by zhoaojiping liuniukeji.com <QQ: 17620286>
+ */
+namespace Payment\Controller;
+use Common\Controller\CommonController;
+
+class WxPayController extends CommonController {
+
+    // 定单支付回调
+    public function wxNotify() {
+        require_once("Plugins/WxPay/WxPay.php");
+        $wxPay = new \WxPay();
+
+        //验证是否是支付宝发送
+        $flag = $wxPay->WxPayNotifyCheck();
+        LL($flag,'./log1.txt');
+        //验证成功
+        if ($flag['status']) {
+            if ($flag['data']['return_code'] == 'SUCCESS' && $flag['data']['result_code'] == 'SUCCESS') {
+                $out_trade_no = trim($flag['data']['out_trade_no']);//商家订单号
+                LL($out_trade_no,'./log1.txt');
+                $total_amount = trim($flag['data']['total_fee']); //支付的金额
+                $trade_no = trim($flag['data']['transaction_id']); //商户订单号
+                //成功后的业务逻辑处理
+                $result = D('Common/PayReturn')->paySuccess($out_trade_no, fen_to_yuan($total_amount), $trade_no, 2);
+                if ($result['status'] == 1) {
+                    $r_arr['return_code'] = 'SUCCESS';
+                    $r_arr['return_msg'] = '回调成功';
+                    echo $wxPay->arrayToXml($r_arr);
+                    die;
+                }
+            }
+        }
+        $r_arr['return_code'] = 'FAIL';
+        $r_arr['return_msg'] = '回调失败';
+        echo $wxPay->arrayToXml($r_arr);
+        die;
+    }
+    
+
+}
