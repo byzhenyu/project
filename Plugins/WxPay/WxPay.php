88842c9df3097eb94a6084fb711ababc9215d700
jicy <510434563@qq.com>
2018-10-08 13:41:59 +0800

    GetOpenidFromMp

--- Plugins/WxPay/WxPay.php
+++ Plugins/WxPay/WxPay.php
@@ -428,7 +428,23 @@ class WxPay {
     public function GetOpenidFromMp($code)
     {
         $url = $this->__CreateOauthUrlForOpenid($code);
-        $res = $this->postXmlCurl('',$url);
+        //初始化curl
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->curl_timeout);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+//        if(WxPayConfig::CURL_PROXY_HOST != "0.0.0.0"
+//            && WxPayConfig::CURL_PROXY_PORT != 0){
+//            curl_setopt($ch,CURLOPT_PROXY, WxPayConfig::CURL_PROXY_HOST);
+//            curl_setopt($ch,CURLOPT_PROXYPORT, WxPayConfig::CURL_PROXY_PORT);
+//        }
+        //运行curl，结果以jason形式返回
+        $res = curl_exec($ch);
+        curl_close($ch);
         //取出openid
         $data = json_decode($res,true);
         $this->data = $data;
