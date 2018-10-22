438451542a5b57d49de533e04457ba5d1aa5ab96
tianzj <2413877061@qq.com>
2018-10-22 15:10:26 +0800

    1

---
+++ Application/Admin/Model/UserTagsModel.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class UserTagsModel extends Model{
+
+    protected function _before_insert(&$data, $option){
+    }
+
+    protected function _before_update(&$data, $option){
+    }
+}
