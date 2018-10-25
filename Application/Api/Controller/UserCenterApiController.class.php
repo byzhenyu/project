b51e0a01dabcc501ba3b7b7dd2baa16f0c87e56f
tianzj <2413877061@qq.com>
2018-10-25 08:33:18 +0800

    1

--- Application/Api/Controller/UserCenterApiController.class.php
+++ Application/Api/Controller/UserCenterApiController.class.php
@@ -280,6 +280,7 @@ class UserCenterApiController extends ApiUserCommonController{
         else{
             $where['a.question_type'] = 0;
         }
+        $where['_string'] = 'or user_id = '.UID;
         if ($keywords) $where['question_title'] = array('like', '%' . $keywords . '%');
         $model = D('Admin/Question');
         $field = 'u.nickname,u.head_pic,a.id,a.like_number,a.browse_number,a.answer_number,a.add_time,a.question_title';
