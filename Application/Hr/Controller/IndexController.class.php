287111cbe46d1cc4867d74175016fde860168dfd
tianzj <2413877061@qq.com>
2018-11-02 11:15:14 +0800

    1

--- Application/Hr/Controller/IndexController.class.php
+++ Application/Hr/Controller/IndexController.class.php
@@ -52,7 +52,7 @@ class IndexController extends HrCommonController {
 
         $where['status'] = 1;
         $where['is_post'] = array('lt', 2);
-        $list = $recruit_model->getRecruitList($where,'id, position_name, recruit_num, commission, add_time, job_area, position_name, welfare, ');
+        $list = $recruit_model->getRecruitList($where,'id, position_name, recruit_num, commission, add_time, job_area, position_name, welfare');
         p($list);
         $this->display();
     }
