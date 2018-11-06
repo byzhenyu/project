e5cb1cfcb3d3caf45f62af77b37a33f42bab759e
tianzj <2413877061@qq.com>
2018-11-06 13:27:43 +0800

    1

--- Application/Api/Controller/UserCenterApiController.class.php
+++ Application/Api/Controller/UserCenterApiController.class.php
@@ -1833,8 +1833,8 @@ class UserCenterApiController extends ApiUserCommonController{
      */
     public function recharge() {
         $recharge_money = I('recharge_money', '');
-        if (!$recharge_money || $recharge_money < 500) {
-            $this->apiReturn(V(0, '充值金额不能小于500！'));
+        if (!$recharge_money || $recharge_money < 1) {
+            $this->apiReturn(V(0, '充值金额不能小于1元！'));
         }
         $code = I('wx_code', '');
         if (!$code) {
