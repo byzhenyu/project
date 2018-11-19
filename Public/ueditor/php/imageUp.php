2208710a6e072915ecd2f230df5f502ba3b5ffa7
tianzj <2413877061@qq.com>
2018-11-19 16:59:24 +0800

    1

--- Public/ueditor/php/imageUp.php
+++ Public/ueditor/php/imageUp.php
@@ -22,7 +22,7 @@
         "maxSize" => 1000, //单位KB
         "allowFiles" => array(".gif", ".png", ".jpg", ".jpeg", ".bmp"),
         "returnPath" => 'https://'.$_SERVER['HTTP_HOST'].'/Uploads/Picture/Detail/', //返回路径
-        "imageCompressEnable" => false,
+        "compressEnable" => false,
     );
 
     //生成上传实例对象并完成上传
